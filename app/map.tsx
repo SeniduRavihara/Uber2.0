611d15d63aa313a06b311d195627bd456d22bed6
SeniduRavihara <senidumee16@gmail.com>
2025-04-04 22:47:55 +0530

    test

--- app/map.tsx
+++ app/map.tsx
@@ -1,13 +1,14 @@
+import GMap from '@/components/GMap'
 import { View, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 const MapScreen = () => {
   return (
-    <SafeAreaView className='bg-black flex-1'>
-      <View className='h-1/2 bg-red-500'>
-
+    <SafeAreaView className='flex-1'>
+      <View className='h-1/2 '>
+         <GMap />
       </View>
 
-      <View className='h-1/2 bg-blue-500'>
+      <View className='h-1/2 '>
         <Text className='text-white'>I'm the map screen</Text>
       </View>
     </SafeAreaView>
