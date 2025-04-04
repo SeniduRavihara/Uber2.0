c7afe8594f2298c964a44919b7ad3d3e31c9014f
SeniduRavihara <senidumee16@gmail.com>
2025-04-04 17:00:28 +0530

    using react-navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,30 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 import { Provider } from "react-redux";
-import { store } from "../store";
 import "../global.css";
+import { store } from "../store";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "@/screens/HomeScreen";
+import MapScreen from "@/screens/MapScreen";
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+const Stack = createStackNavigator({
+  screens: {
+    Home: HomeScreen,
+    Map: MapScreen,
+  },
+});
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -36,13 +44,27 @@ export default function RootLayout() {
 
   return (
     <Provider store={store}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+      {/* <NavigationContainer> */}
+      <SafeAreaProvider>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Map"
+            component={MapScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </SafeAreaProvider>
+      {/* </NavigationContainer> */}
+      <StatusBar style="auto" />
     </Provider>
   );
 }
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,21 @@
+import NavOptions from "@/components/NavOptions";
+import { Image, StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="bg-white w-screen h-screen">
+      <View className="p-5">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+        />
+
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
+}
+
