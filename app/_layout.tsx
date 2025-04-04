53c9c6047e2e836e785563b0a4b3f61308cc2dc6
SeniduRavihara <senidumee16@gmail.com>
2025-04-04 22:09:32 +0530

    remove unnessosary things

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,20 +3,16 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
+import "react-native-get-random-values";
 import "react-native-reanimated";
 import { Provider } from "react-redux";
 import "../global.css";
 import { store } from "../store";
-import "react-native-get-random-values";
-
-
-import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
--- app/map.tsx
+++ app/map.tsx
@@ -2,8 +2,14 @@ import { View, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 const MapScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>MapScreen</Text>
+    <SafeAreaView className='bg-black flex-1'>
+      <View className='h-1/2 bg-red-500'>
+
+      </View>
+
+      <View className='h-1/2 bg-blue-500'>
+        <Text className='text-white'>I'm the map screen</Text>
+      </View>
     </SafeAreaView>
   )
 }
